f892098b0ca89cc1a8c22d78b12eae1d0a4e63eb
mikeatok <miatokhala@s6c.mlp.college>
2025-05-12 09:41:23 +0100

    setting up the virtual environment

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask,render_template
+PORT= 2000
+app = Flask(__name__,template_folder='template')
+@app.route("/") 
+def index(): 
+	return render_template("home.html")
+if __name__ == "__main__": 
+	app.run(debug=True, host = '0.0.0.0', port=PORT) 
+	
