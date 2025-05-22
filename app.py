df4a7e10af05d4af0d5c2c74d6ad4710039e1599
mikeatok <miatokhala@s6c.mlp.college>
2025-05-22 14:01:18 +0100

    just keeping up to date

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ PORT= 2000
 app = Flask(__name__,template_folder='template')
 @app.route("/") 
 def index(): 
-	return render_template("stories.html")
+	return render_template("home.html")
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
 	
