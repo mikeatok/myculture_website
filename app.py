8de53fc5dde7914498769437e32d4136812eb2f3
mikeatok <miatokhala@s6c.mlp.college>
2025-05-26 10:55:32 +0100

    making sure the hrefs are working

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__,template_folder='template')
 
 @app.route("/") 
 def index(): 
-	return render_template("home.html")
+	return render_template("stories.html")
 	return render_template("stories.html")
 
 @app.route("/recipes") 
